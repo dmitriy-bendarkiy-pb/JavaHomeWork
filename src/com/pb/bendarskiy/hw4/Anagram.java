bac8efc7c2b3d19aafd531c799782633996dd1db
Dmitriy Bendarskiy <dmitrij.bendarskij@privatbank.ua>
2021-10-31 00:06:25 +0300

    HW4

---
+++ src/com/pb/bendarskiy/hw4/Anagram.java
@@ -0,0 +1,42 @@
+package com.pb.bendarskiy.hw4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagram {
+    public Anagram() {
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите первое предложение:");
+        String Text1 = scan.nextLine();
+        Text1 = Text1.replaceAll("\\p{Punct}+|\\s+", "");
+        System.out.println("Введите втрое предложение:");
+        String Text2 = scan.nextLine();
+        Text2 = Text2.replaceAll("\\p{Punct}+|\\s+", "");
+        boolean Result = isAnagram(Text1, Text2);
+        if (!Result) {
+            System.out.println("Ввведенные предложения не являются анаграммой");
+        } else {
+            System.out.println("Введенные предложения являются анаграммой");
+        }
+
+    }
+
+    static boolean isAnagram(String t1, String t2) {
+        if (t1.length() != t2.length()) {
+            return false;
+        } else {
+            t1 = t1.toLowerCase();
+            t2 = t2.toLowerCase();
+            char[] c1 = t1.toCharArray();
+            char[] c2 = t2.toCharArray();
+            Arrays.sort(c1);
+            Arrays.sort(c2);
+            String s1 = new String(c1);
+            String s2 = new String(c2);
+            return s1.equals(s2);
+        }
+    }
+}
