6ff16a391244b58fd24c14c128ff5dd018028843
Dmitriy Bendarskiy <dmitrij.bendarskij@privatbank.ua>
2021-11-28 20:04:52 +0200

    HW10

--- src/com/pb/bendarskiy/hw10/Main.java
+++ src/com/pb/bendarskiy/hw10/Main.java
@@ -12,9 +12,9 @@ public class Main {
         floatNumbers.add((float) 14);
         System.out.println("Element with index 1: " + floatNumbers.get(1));
         System.out.println("Length after 5 added element: " + floatNumbers.length());
-        System.out.println("Average: " + floatNumbers.average());
-        System.out.println("Sum: " + floatNumbers.sum());
-        System.out.println("Max: " + floatNumbers.max());
+        System.out.println("Среднее арифметическое среди элементов: " + floatNumbers.average());
+        System.out.println("Сумма элементов: " + floatNumbers.sum());
+        System.out.println("Максимальный элемент: " + floatNumbers.max());
 
         System.out.println("_____________________________");
 
@@ -27,9 +27,9 @@ public class Main {
         intNumbers.add(13);
         System.out.println("Element with index 1: " + intNumbers.get(1));
         System.out.println("Length after 1 added element: " + intNumbers.length());
-        System.out.println("Average: " + intNumbers.average());
-        System.out.println("Sum: " + intNumbers.sum());
-        System.out.println("Max: " + intNumbers.max());
+        System.out.println("Среднее арифметическое среди элементов: " + intNumbers.average());
+        System.out.println("Сумма элементов: " + intNumbers.sum());
+        System.out.println("Максимальный элемент: " + intNumbers.max());
         System.out.println("_____________________________");
 
         System.out.println("Try to add 5th element in Integer NumBox:");
